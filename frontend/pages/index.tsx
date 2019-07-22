e76c913c2a0870d1416135b1d11cec1cc35621ae
Robert A <daspanisharmada@gmail.com>
2019-07-21 17:44:51 -0700

    index page

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { NextPage } from 'next';
+
+const IndexPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello Friends</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
